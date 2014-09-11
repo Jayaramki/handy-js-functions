405c4abf534dd53893e6ae4a9be09db87e0c03da
Kamran Ahmed <kamranahmed.se@gmail.com>
2014-09-11 12:55:37 +0500

    Refactored the DOM file

--- functions/dom.js
+++ functions/dom.js
@@ -1,40 +1,48 @@
-// Do something when the page end is reached
-// Pass the optional offsetBottom param that will check if that much pixels are left towards the apge end
-function isPageEndReached ( offsetBottom ) {
-	
-	if (typeof offsetBottom == "undefined") {
-		offsetBottom = 0;
-	}
+var HandyDom = {
 
-	return ($(window).scrollTop() + $(window).height() + offsetBottom) >= $(document).height();
-}
+  /**
+   * Checks if the page end is reached or not
+   * @param  {number}  offsetBottom Optional numeric value that checks if the bottom is that much pixels away from the current position
+   * @return {Boolean}              true if the page end is reached and false otherwise
+   */
+  isPageEndReached : function ( offsetBottom ) {
+  	
+  	if (typeof offsetBottom == "undefined") {
+  		offsetBottom = 0;
+  	}
 
-// To support the older browsers such as <= IE8
-// Check the browser, and call this function if required. It'll automatically fix the placeholders
-function fixPlaceholders()
-{
-   var holderInputs = $("input[placeholder]");
+  	return ($(window).scrollTop() + $(window).height() + offsetBottom) >= $(document).height();
+  },
 
-   holderInputs.each(function(){
-        var name=$(this).attr("name");
-        var newInput = $("<input type='text' name='"+name+"' value='"+name+" goes here'>");
+  /**
+   * Replaces the placeholder'ed' input elements with a javascript alternative to support older browsers
+   */
+  fixPlaceholders : function ()
+  {
+     var holderInputs = $("input[placeholder]");
 
-        $(this).replaceWith(newInput);
-        var defaultValue = name + " goes here";
+     holderInputs.each(function(){
+          var name=$(this).attr("name");
+          var newInput = $("<input type='text' name='"+name+"' value='"+name+" goes here'>");
 
-        newInput.on('focus', function() {
-            // If this value of the input equals our sample,
-            // hide it when the user clicks on it.
+          $(this).replaceWith(newInput);
+          var defaultValue = name + " goes here";
 
-            if(this.value === defaultValue)
-               this.value = '';
-        });
+          newInput.on('focus', function() {
+              // If this value of the input equals our sample,
+              // hide it when the user clicks on it.
 
-        newInput.on('blur', function() {
-            // When they click off of the input, if
-            // the value is blank, bring back the sample.
-            if(this.value === '')
-                this.value = defaultValue;
-        });
-   });
-}
+              if(this.value === defaultValue)
+                 this.value = '';
+          });
+
+          newInput.on('blur', function() {
+              // When they click off of the input, if
+              // the value is blank, bring back the sample.
+              if(this.value === '')
+                  this.value = defaultValue;
+          });
+     });
+  }
+  
+};
