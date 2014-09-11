0fe3cf961c3204476a63697a64e2bd88b1f021bf
Kamran Ahmed <kamranahmed.se@gmail.com>
2014-09-11 12:48:09 +0500

    Refactored the dates file

--- functions/dates.js
+++ functions/dates.js
@@ -1,81 +1,113 @@
-// Checks if the argument passed is a valid date
-function isDate ( dateArg ) {
-	var t =  ( dateArg instanceof Date ) ? dateArg : (new Date( dateArg ));
-	return !isNaN(t.valueOf());
-}
+var HandyDate = {
 
-// Checks if year is a Leap year
-function isLeapYear (year) {
-	return (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0); 
+	/**
+	 * Checks if the argument passed is a valid date or not
+	 * @param  {string|Date object}  dateArg Argument that is to be checked
+	 * @return {Boolean}         true if the argument is a valid date and false otherwise
+	 */
+	isDate : function ( dateArg ) {
+		var t =  ( dateArg instanceof Date ) ? dateArg : (new Date( dateArg ));
+		return !isNaN(t.valueOf());
+	},
+
+	/**
+	 * Checks if the year passed is a leap year or not
+	 * @param  {number}  year Numeric value representing the year that is to be checked
+	 * @return {Boolean}      True if the year is a leap year and False otherwise
+	 */
+	isLeapYear : function (year) {
+		return (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0); 
+	},
+
+	/**
+	 * From an array of dates, returns the maximum date
+	 * @param  {array} dates Array containing the strings of dates
+	 * @return {string}       Returns the element of the array that has the maximum date
+	 */
+	maxDate : function ( dates ) {
+
+		var maxDate = dates[0],
+			maxDateObj = new Date( dates[0] );
+
+		dates.forEach( function ( date, index ) {
+			if ( new Date( date ) > maxDateObj ) {
+				maxDate = date;
+				maxDateObj = new Date( date );
+			};
+		});
+
+		return maxDate;
+	},
+
+	/**
+	 * Returns the minimum date of the array of dates
+	 * @param  {array} dates Array containing the strings of dates
+	 * @return {string}       Returns the array element that has the minimum date
+	 */
+	minDate : function ( dates ) {
+
+		var minDate = dates[0],
+			minDateObj = new Date( dates[0] );
+
+		dates.forEach( function ( date, index ) {
+			if ( new Date( date ) < minDateObj ) {
+				minDate = date;
+				minDateObj = new Date( date );
+			};
+		});
+
+		return minDate;
+	},
+
+	/**
+	 * Checks if the date range is valid or not or in other words, checks if the first date is smaller than the second or not
+	 * @param  {string}  minDate String representing the starting date of the range
+	 * @param  {string}  maxDate String representing the ending date of the range
+	 * @return {Boolean}         True if the range is valid and false otherwise
+	 */
+	isValidRange : function ( minDate, maxDate ) {
+		return ( new Date( minDate ) <= new Date( maxDate ) );
+	},
+
+	/**
+	 * Returns the mysql format of the passed date
+	 * @param  {string} dateArg string representing the date which is to be converted to mysql date
+	 * @return {string}         String representing the date string in the mysql format
+	 */
+	getMySqlDateFormat : function ( dateArg ) {	
+		
+		var date = new Date( dateArg );
+
+		var dd = date.getDate();
+		var mm = date.getMonth() + 1; //January is 0!
+		var yyyy = date.getFullYear();
+
+		dd = ( dd < 10 ) ? ( '0' + dd ) : dd;
+		mm = ( mm < 10 ) ? ( '0' + mm ) : mm;
+		
+		date = yyyy + '/' + mm + '/' + dd;
+
+		return date;
+	},
+
+	/**
+	 * Returns the current date in MySQL format
+	 * @return {string} String representing the current date in Mysql format
+	 */
+	getCurrentDate : function () {
+		
+		var today = new Date();
+
+		var dd = today.getDate();
+		var mm = today.getMonth() + 1; //January is 0!
+		var yyyy = today.getFullYear();
+
+		dd = ( dd < 10 ) ? ( '0' + dd ) : dd;
+		mm = ( mm < 10 ) ? ( '0' + mm ) : mm;
+
+		today = yyyy + '/' + mm + '/' + dd;
+
+		return today;
+	}
+
 };
-
-// Returns the maximum date of the passed array of date strings
-function maxDate ( dates ) {
-
-	var maxDate = dates[0],
-		maxDateObj = new Date( dates[0] );
-
-	dates.forEach( function ( date, index ) {
-		if ( new Date( date ) > maxDateObj ) {
-			maxDate = date;
-			maxDateObj = new Date( date );
-		};
-	});
-
-	return maxDate;
-}
-
-// Returns the minimum date of the passed array of date strings
-function minDate ( dates ) {
-
-	var minDate = dates[0],
-		minDateObj = new Date( dates[0] );
-
-	dates.forEach( function ( date, index ) {
-		if ( new Date( date ) < minDateObj ) {
-			minDate = date;
-			minDateObj = new Date( date );
-		};
-	});
-
-	return minDate;
-}
-
-// Checks if the date range is valid
-function isValidRange ( minDate, maxDate ) {
-	return ( new Date( minDate ) <= new Date( maxDate ) );
-}
-
-// Returns mysql date format e.g. 2014/09/07
-function getMySqlDateFormat ( dateArg ) {	
-	
-	var date = new Date( dateArg );
-
-	var dd = date.getDate();
-	var mm = date.getMonth() + 1; //January is 0!
-	var yyyy = date.getFullYear();
-
-	dd = ( dd < 10 ) ? ( '0' + dd ) : dd;
-	mm = ( mm < 10 ) ? ( '0' + mm ) : mm;
-	
-	date = yyyy + '/' + mm + '/' + dd;
-
-	return date;
-}
-
-// Returns the current date
-function getCurrentDate () {
-	
-	var today = new Date();
-
-	var dd = today.getDate();
-	var mm = today.getMonth() + 1; //January is 0!
-	var yyyy = today.getFullYear();
-
-	dd = ( dd < 10 ) ? ( '0' + dd ) : dd;
-	mm = ( mm < 10 ) ? ( '0' + mm ) : mm;
-
-	today = yyyy + '/' + mm + '/' + dd;
-
-	return today;
-}
