6c7fcbfecf5319239dc4c24ac08e5fe4563c9e37
kamran ahmed <kamranahmed.se@gmail.com>
2014-09-14 18:23:10 +0500

    Added the getParamValue function in the URL module

--- functions/url.js
+++ functions/url.js
@@ -5,5 +5,18 @@ var Url = {
 	 */
 	getWindowUrl : function () {
 		return window.location.href;
+	},
+
+	/**
+	 * Gets the specific query string value from the URL
+	 * @param  {string} name Parameter name, whose value is to be retrieved from the query string
+	 * @return {mixed} 		 null if the parameter is not found in the query string or the value if found.
+	 */
+	getParamValue : function ( name ) {
+	    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+	    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+	        results = regex.exec(location.search);
+	        
+	    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 	}
 };
