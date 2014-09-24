8d9c1fadf576cd559318a2f3d09cb9d211bc6aa1
CJRamki@MCS <cj.ramki@yahoo.com>
2014-09-24 16:28:29 +0530

    Update string.js

--- functions/string.js
+++ functions/string.js
@@ -171,15 +171,5 @@ var HandyString = {
   getWordsCount : function ( string ) {
     var words = string.split(' ');
     return words.length;
-  },
-  
-    /**
-   * Returns the number of characters in a string
-   * @param  {str} string String whose number of characters is to be counted
-   * @return {integer}        Number representing the number of characters in that string
-   */
-  getCharCount : function ( string ) {
-    var chars = string.split('');
-    return chars.length;
   }
 }
